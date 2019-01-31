6b51e1f13a271f012263d9ae20c1decc62c56df8
kavics <gyeby5@gmail.com>
2019-01-31 01:28:58 +0100

    Feature/accesstoken (#8)
    * Integration test project initiative for the ContentRepository.
    
    * Add AccessToken integration tests.
    
    * Fix custom script usage
    
    * Rename tests. Remove an UNDONE
    
    * Set default sql implementation for accesstoken provider.

---
+++ src/SenseNet.ContentRepository.IntegrationTests/Initializer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SenseNet.Diagnostics;
+
+namespace SenseNet.ContentRepository.IntegrationTests
+{
+    public class Initializer
+    {
+        [AssemblyInitialize]
+        public static void StartAllTests(TestContext testContext)
+        {
+            SnTrace.SnTracers.Clear();
+            SnTrace.SnTracers.Add(new SnFileSystemTracer());
+            SnTrace.SnTracers.Add(new SnDebugViewTracer());
+            SnTrace.Test.Enabled = true;
+            SnTrace.Test.Write("------------------------------------------------------");
+        }
+    }
+}
