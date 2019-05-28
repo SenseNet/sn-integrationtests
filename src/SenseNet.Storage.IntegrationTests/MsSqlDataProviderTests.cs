767a7e0490cd0b0c947245cd466e56affa482ec6
Zoltán Gyebrovszki <kavics@sensenet.com>
2019-05-29 01:23:29 +0200

    TreeSize tests

--- src/SenseNet.Storage.IntegrationTests/MsSqlDataProviderTests.cs
+++ src/SenseNet.Storage.IntegrationTests/MsSqlDataProviderTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SenseNet.ContentRepository.Storage.Data;
+using SenseNet.Storage.Data.MsSqlClient;
 using SenseNet.Tests.Implementations;
 
 namespace SenseNet.Storage.IntegrationTests
@@ -13,12 +14,53 @@ namespace SenseNet.Storage.IntegrationTests
         private static ITestingDataProviderExtension TDP => DataStore.GetDataProviderExtension<ITestingDataProviderExtension>();
 
         [TestMethod]
-        public async Task MsSqlDP_____()
+        public async Task MsSqlDP_TreeSize_Root()
         {
             await StorageTest(async () =>
             {
+                // ACTION
                 var size = await DP.GetTreeSizeAsync("/Root", true);
-                Assert.AreEqual(42, size);
+
+                // ASSERT
+                var expectedSize = await MsSqlProcedure.ExecuteScalarAsync(
+                    "SELECT SUM(Size) FROM Files", value => (long)value);
+                Assert.AreEqual(expectedSize, size);
+            });
+        }
+        [TestMethod]
+        public async Task MsSqlDP_TreeSize_Subtree()
+        {
+            await StorageTest(async () =>
+            {
+                // ACTION
+                var size = await DP.GetTreeSizeAsync("/Root/System/Schema/ContentTypes/GenericContent/Folder", true);
+
+                // ASSERT
+                var sql = @"SELECT SUM(Size) FROM Files f
+    JOIN BinaryProperties b ON b.FileId = f.FileId
+    JOIN Versions v ON v.VersionId = b.VersionId
+    JOIN Nodes n on n.NodeId = v.NodeId
+WHERE Path LIKE '/Root/System/Schema/ContentTypes/GenericContent/Folder%'";
+                var expectedSize = await MsSqlProcedure.ExecuteScalarAsync(sql, value => (long)value);
+                Assert.AreEqual(expectedSize, size);
+            });
+        }
+        [TestMethod]
+        public async Task MsSqlDP_TreeSize_Item()
+        {
+            await StorageTest(async () =>
+            {
+                // ACTION
+                var size = await DP.GetTreeSizeAsync("/Root/System/Schema/ContentTypes/GenericContent/Folder", false);
+
+                // ASSERT
+                var sql = @"SELECT SUM(Size) FROM Files f
+    JOIN BinaryProperties b ON b.FileId = f.FileId
+    JOIN Versions v ON v.VersionId = b.VersionId
+    JOIN Nodes n on n.NodeId = v.NodeId
+WHERE Path = '/Root/System/Schema/ContentTypes/GenericContent/Folder'";
+                var expectedSize = await MsSqlProcedure.ExecuteScalarAsync(sql, value => (long)value);
+                Assert.AreEqual(expectedSize, size);
             });
         }
     }
